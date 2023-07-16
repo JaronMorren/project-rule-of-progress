399392e76e19eedb3a2c0cff14ae723523077d21
Jaron Morren <jaronmorren@gmail.com>
2023-07-15 22:30:08 -0300

    feat:Added activities, weight and grade scanner methods

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,20 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a quantidade de atividades para cadastrar: ");
+    int activitiesNumber = Integer.parseInt(scanner.nextLine());
+
+    for (int index = 1; index <= activitiesNumber; index += 1) {
+      System.out.print("Digite o nome da atividade " + index + ": ");
+      String activityName = scanner.nextLine();
+
+      System.out.print("Digite o peso da atividade " + index + ": ");
+      int activityWeight = Integer.parseInt(scanner.nextLine());
+
+      System.out.print("Digite a nota obtida para " + activityName + ": ");
+      int activityGrade = Integer.parseInt(scanner.nextLine());
+    }
+    scanner.close();
   }
 }
