40ae3014cba437bbd804911cd1e21bb2971a125d
Jaron Morren <jaronmorren@gmail.com>
2023-07-17 21:12:16 -0300

    Feat:Implemented show final grade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -14,6 +14,8 @@ public class App {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Digite a quantidade de atividades para cadastrar: ");
     int activitiesNumber = Integer.parseInt(scanner.nextLine());
+    float totalGrades = 0;
+    int totalWeight = 0;
 
     for (int index = 1; index <= activitiesNumber; index += 1) {
       System.out.print("Digite o nome da atividade " + index + ": ");
@@ -24,6 +26,25 @@ public class App {
 
       System.out.print("Digite a nota obtida para " + activityName + ": ");
       int activityGrade = Integer.parseInt(scanner.nextLine());
+
+      totalGrades += (activityWeight * activityGrade);
+      totalWeight += activityWeight;
+    }
+    float finalGrade = 0;
+    if (totalWeight != 0) {
+      finalGrade = totalGrades / totalWeight;
+    } else {
+      System.out.print("A pontuação final não pode ser calculada, pois o peso total é zero.");
+    }
+
+    if (finalGrade >= 85) {
+      System.out.print("Parabéns! Você alcançou " + finalGrade + "%! E temos o prazer "
+              + "de informar que você obteve aprovação!");
+    } else {
+      System.out.print("Lamentamos informar que, "
+              + "com base na sua pontuação alcançada neste período, "
+              + finalGrade
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
     scanner.close();
   }
